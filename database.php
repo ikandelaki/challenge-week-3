32522569c575b669bf98bb9bcd83b0f831113637
kandelseik <iraklikandelaki2@gmail.com>
2022-05-27 20:23:07 +0400

    practically finished working

---
+++ database.php
@@ -0,0 +1,6 @@
+<?php
+// Connection with database
+$pdo = new PDO('mysql:host=localhost;port=3306;dbname=anime_list', 'root', '');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+?>
